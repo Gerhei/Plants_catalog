355007966912f840d7738244b0c598562f418770
Andrei Pavlov <andrew_p@intime.digital>
2022-01-16 01:44:28 +0300

    Created models.

--- main_app/plants/admin.py
+++ main_app/plants/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class PlantsAdmin(admin.ModelAdmin):
+    list_display = ('id','name','time_create','time_update')
+    list_display_links = ('id','name')
+    search_fields = ('id','name')
+
+class DescriptionsAdmin(admin.ModelAdmin):
+    list_display = ('id','category','plant','time_create','time_update')
+    list_display_links = ('id','category','plant')
+    search_fields = ('id','category','plant','text')
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id','name','time_create','time_update')
+    list_display_links = ('id','name')
+    search_fields = ('id','name')
+
+class TaxonsAdmin(admin.ModelAdmin):
+    list_display = ('id','name','time_create','time_update')
+    list_display_links = ('id','name')
+    search_fields = ('id','name')
+
+admin.site.register(Plants,PlantsAdmin)
+admin.site.register(Descriptions,DescriptionsAdmin)
+admin.site.register(Categories,CategoriesAdmin)
+admin.site.register(Taxons,TaxonsAdmin)
--- main_app/plants/models.py
+++ main_app/plants/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=64, unique=True,verbose_name='Наименование')
+    time_create = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Категория"
+        verbose_name_plural="Категории"
+        ordering=['name']
+
+class Taxons(models.Model):
+    name = models.CharField(max_length=64, unique=True,verbose_name='Наименование')
+    rang = models.CharField(max_length=64,verbose_name='Ранг')
+    time_create = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Таксон"
+        verbose_name_plural="Таксоны"
+        ordering=['name']
+
+class Plants(models.Model):
+    name=models.CharField(max_length=64,unique=True,verbose_name='Название')
+    image=models.ImageField(upload_to="./static",blank=True)
+    time_create=models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    time_update=models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+    categories=models.ManyToManyField(Categories)
+    taxons=models.ManyToManyField(Taxons)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Растение"
+        verbose_name_plural="Растения"
+        ordering=['name']
+
+class Descriptions(models.Model):
+    category=models.CharField(max_length=64,verbose_name='Категория')
+    text=models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to="./static",blank=True)
+    time_create=models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    time_update=models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+    plant=models.ForeignKey(Plants,on_delete=models.CASCADE,verbose_name='Растение')
+
+    def __str__(self):
+        return self.plant.name+' '+self.category
+
+    class Meta:
+        verbose_name="Описание"
+        verbose_name_plural="Описания"
+        ordering=['category']
