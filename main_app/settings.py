07760c6985a1ad3de8537ffe0c301e5a88f8391d
Andrei Pavlov <andrio.pavlov@gmail.com>
2022-06-05 01:07:45 +0300

    Set compressed static files storage.

--- main_app/settings.py
+++ main_app/settings.py
@@ -215,6 +215,7 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
